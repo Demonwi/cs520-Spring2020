5518a8bb4f310131b6e188716a79f0f52c3a57e9
Jerry Fu <jerry.fu98@gmail.com>
2021-04-03 20:16:23 -0400

    Update RowGameApp.java

--- threeinarow/src/RowGameApp.java
+++ threeinarow/src/RowGameApp.java
@@ -1,13 +1,30 @@
 import controller.RowGameController;
+import java.util.Scanner;
 
-
-public class RowGameApp 
+public class RowGameApp
 {
-    /**                                                                             
+    /**
      * Starts a new game in the GUI.
      */
     public static void main(String[] args) {
-	RowGameController game = new RowGameController();
-	game.startUp();
+      Scanner myObj = new Scanner(System.in);
+      String input = "0";
+      while(true){
+        System.out.println("Enter 1 for Tic Tac Toe and 2 for Three in a Row");
+        input = myObj.nextLine();
+        if(input == "1"){
+          RowGameController game = new RowGameController();
+        	game.startUp();
+          break;
+        }else if(input == "2"){
+          RowGameController game = new RowGameController();
+        	game.startUp();
+          break;
+        }else{
+          System.out.println("Input not recognized");
+        }
+      }
+    	RowGameController game = new RowGameController();
+    	game.startUp();
     }
 }
